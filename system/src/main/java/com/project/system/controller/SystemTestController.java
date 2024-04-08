39e7c98006d0e14e59db3a06a0d0f9fff08b00c1
heja <wbl_d1c@163.com>
2024-04-08 23:18:07 +0800

    系统菜单添加

---
+++ system/src/main/java/com/project/system/controller/SystemTestController.java
@@ -0,0 +1,14 @@
+package com.project.system.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+  *@author Duanzp
+  *@since 2024-04-07 22:57:41
+  */
+@RestController
+@RequestMapping("/system")
+public class SystemTestController {
+}
